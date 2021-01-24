280c52b098e003d11ddb5c3267bddb2856603750
Oscar Spierings <oscar.spierings@gmail.com>
2021-01-24 02:40:12 +0100

    updated root escalation

--- INFO.sh
+++ INFO.sh
@@ -4,10 +4,14 @@
 source /pkgscripts-ng/include/pkg_util.sh
 
 package="r8152"
-version="2.14.0-2"
-displayname="RTL8152/RTL8153 driver"
-maintainer="bb-qq"
+version="2.14.0-8"
+os_min_ver="7.0-40000"
+displayname="RTL815X driver"
+maintainer="bb-qq (Beatskip Fork)"
 arch="$(pkg_get_platform)"
-description="Realtek RTL8152/RTL8153 Based USB Ethernet Adapters driver."
+description="Realtek RTL815X Based USB Ethernet Adapters driver. For DSM 7.0 Beta"
+beta="yes"
+install_type="package"
+thirdparty="no"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
